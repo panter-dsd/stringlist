312dee745987dce13b9aeaa078100ebd30297ce1
PanteR <panter.dsd@gmail.com>
2019-06-08 08:33:54 +0300

    Much files was added.

---
+++ pkg/stringlist/stringlist.go
@@ -0,0 +1,42 @@
+package stringlist
+
+import "sort"
+
+// StringList ...
+type StringList []string
+
+// Sort ...
+func (s *StringList) Sort() *StringList {
+	sort.Strings(*s)
+	return s
+}
+
+// IsEmpty ...
+func (s *StringList) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+// IsEqual ...
+func IsEqual(a, b StringList) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Contains ...
+func (s *StringList) Contains(str string) bool {
+	for _, e := range *s {
+		if e == str {
+			return true
+		}
+	}
+	return false
+}
---
+++ test/stringlist_test.go
@@ -0,0 +1,58 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	sl "github.com/panter-dsd/stringlist/pkg/stringlist"
+)
+
+func TestStringList_SortEmpty(t *testing.T) {
+	s := sl.StringList{}
+	s.Sort()
+	assert.Empty(t, s)
+}
+
+func TestStringList_SortSingleElement(t *testing.T) {
+	a := sl.StringList{"a"}
+	b := a
+	b.Sort()
+	assert.Equal(t, a, b)
+}
+
+func TestStringList_SortNotSorted(t *testing.T) {
+	a := sl.StringList{"b", "a"}
+	a.Sort()
+	assert.Equal(t, sl.StringList{"a", "b"}, a)
+}
+
+func TestStringList_SortSorted(t *testing.T) {
+	a := sl.StringList{"a", "b"}
+	a.Sort()
+	assert.Equal(t, sl.StringList{"a", "b"}, a)
+}
+
+func TestStringList_IsEmpty(t *testing.T) {
+	assert.True(t, (&sl.StringList{}).IsEmpty())
+	assert.False(t, (&sl.StringList{"a"}).IsEmpty())
+}
+
+func TestIsEqual(t *testing.T) {
+	assert.True(t, sl.IsEqual(sl.StringList{}, sl.StringList{}))
+	assert.True(t, sl.IsEqual(sl.StringList{"a"}, sl.StringList{"a"}))
+	assert.True(t, sl.IsEqual(sl.StringList{"a", "b"}, sl.StringList{"a", "b"}))
+
+	assert.False(t, sl.IsEqual(sl.StringList{"a"}, sl.StringList{"b"}))
+	assert.False(t, sl.IsEqual(sl.StringList{"a", "b"}, sl.StringList{"a"}))
+	assert.False(t, sl.IsEqual(sl.StringList{"a", "b"}, sl.StringList{"b", "a"}))
+}
+
+func TestStringList_Contains(t *testing.T) {
+	assert.False(t, (&sl.StringList{}).Contains(""))
+	assert.False(t, (&sl.StringList{}).Contains("a"))
+	assert.False(t, (&sl.StringList{"b", "c"}).Contains("a"))
+
+	assert.True(t, (&sl.StringList{"b", "c"}).Contains("c"))
+	assert.True(t, (&sl.StringList{"c"}).Contains("c"))
+}
